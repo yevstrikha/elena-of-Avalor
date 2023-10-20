028e729565c2b764a5655da2b12e65dca340f5bb
yevstrikha <yevstrikha@gmail.om>
2023-10-19 22:41:25 -0400

    add one book

--- src/scripts/intro.js
+++ src/scripts/intro.js
@@ -1,4 +1,7 @@
-const prevIntro = document.querySelector('.intro_prev');
+
+// document.querySelector('.main_blocks').style.display = 'none';
+let userNameIntro = document.querySelector('#user_name')
+
 const nextIntro = document.querySelector('.intro_next');
 const okayIntro = document.querySelector('.intro_okay');
 let currentImg = document.querySelector('.intro_img img');
@@ -8,28 +11,39 @@ let i = 1;
 function loadIntro() {
     currentImg.setAttribute('src', 'src/images/intro/intro_' + (i) + '.jpg')
 }
+
 okayIntro.style.display = 'none';
-prevIntro.style.display = 'none';
 
 nextIntro.addEventListener('click', nextImgIntro)
-prevIntro.addEventListener('click',prevImgIntro)
+
 function nextImgIntro() {
-    currentImg.setAttribute('src', 'src/images/intro/intro_' + (++i) + '.jpg')
-    prevIntro.style.display = 'block';
+    currentImg.setAttribute('src', 'src/images/intro/intro_' + (++i) + '.jpg');
+
+    if (i === 2) {
+        document.querySelector('#user_name').style.display = 'block';
+    }else if(userNameIntro.value === '' || userNameIntro.value === null){
+        alert('your name');
+        currentImg.setAttribute('src', 'src/images/intro/intro_' + (--i) + '.jpg');
+
+    }
+    else {
+        document.querySelector('#user_name').style.display = 'none';
+    }
+
     if (i === 4) {
         nextIntro.style.display = 'none';
         okayIntro.style.display = 'block';
+        document.querySelector('.intro_text').textContent = `So, ${userNameIntro.value}, here is my younger sister. Are you ready to start?`
+        document.querySelector('.intro_text').style.display = 'block';
+    }else {
+        nextIntro.style.display = 'block';
 
     }
 }
-function prevImgIntro() {
-    nextIntro.style.display = 'block'
-    okayIntro.style.display = 'none';
-    currentImg.setAttribute('src', 'src/images/intro/intro_' + (--i) + '.jpg')
-    prevIntro.style.display = 'block';
-    if (i === 1) {
-        prevIntro.style.display = 'none';
 
-    }
-}
+okayIntro.addEventListener('click', () => {
+    document.querySelector('.intro').style.display = 'none';
+    document.querySelector('header').style.display = 'block';
+    document.querySelector('.main_blocks').style.display = 'flex';
+})
 loadIntro()
--- src/scripts/js.js
+++ src/scripts/js.js
@@ -1,7 +1,7 @@
 const fourNumbers = document.querySelector('.task_four');
 fourNumbers.addEventListener('click', () => {
     document.querySelector('.numbers').style.display = 'block';
-    document.querySelector('.main').style.display = 'none';
+    document.querySelector('.main_blocks').style.display = 'none';
     mainNumbers.innerHTML = ''
     createTask()
 })
@@ -10,7 +10,7 @@ const popUpNo = document.querySelector('.pop_up_no');
 popUpNo.addEventListener('click', () => {
     document.querySelector('.pop_up').style.display = 'none';
     document.querySelector('.numbers').style.display = 'none';
-    document.querySelector('.main').style.display = 'block';
+    document.querySelector('.main_blocks').style.display = 'flex';
     mainNumbers.classList.remove('blur');
 
 
@@ -23,5 +23,46 @@ const popUpYes = document.querySelector('.pop_up_yes');
     mainNumbers.innerHTML = ''
     createTask()
 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+let pages = document.getElementsByClassName('page');
+for(let i = 0; i < pages.length; i++){
+    let page = pages[i];
+    if (i % 2 === 0)
+    {
+        page.style.zIndex = (pages.length-i);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    for(let i = 0; i < pages.length; i++)
+    {
+        pages[i].pageNum = i + 1;
+        pages[i].onclick=function()
+        {
+            if (this.pageNum % 2 === 0)
+            {
+                this.classList.remove('flipped');
+                this.previousElementSibling.classList.remove('flipped');
+            }
+            else
+            {
+                this.classList.add('flipped');
+                this.nextElementSibling.classList.add('flipped');
+            }
+        }
+    }
 })
 
